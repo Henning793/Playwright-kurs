2fec7018619e790174ad5fd99f0e4cfd909eed4b
Henning <henning.kvaloy@gmail.com>
2023-03-02 14:45:36 +0100

    Initial commit for the first challenge

--- tests/bankTest.spec.ts
+++ tests/bankTest.spec.ts
@@ -1,48 +1,14 @@
 import { test, expect } from "@playwright/test";
-import { randomInt } from "crypto";
 
 test.describe("Min første test Suite", () => {
-  test.beforeEach(async ({ page }) => {
-    // Logger på nettbanken.
+  test("Min første test", async ({ page }) => {
+    // Lag først en test som logger inn i nettbanken og gå til forsiden av nettbanken.
+    // Lag en sjekk/test som verifiserer at du er logget inn.
+    // brukernavn: jsmith@demo.io
+    // passord: Demo123!
+    // Bruk dokumentasjonen og hverandre til hjelp:
     await page.goto(
       "http://ec2-13-49-246-53.eu-north-1.compute.amazonaws.com:8080/bank/login"
     );
-    await page.getByPlaceholder("Enter User Name").fill("jsmith@demo.io");
-    await page.getByPlaceholder("Enter Password").fill("Demo123!");
-    await page.getByPlaceholder("Enter Password").press("Enter");
-    // Navigerer til forsiden av banken.
-    await page.goto(
-      "http://ec2-13-49-246-53.eu-north-1.compute.amazonaws.com:8080/bank/home"
-    );
-  });
-
-  test("Min første test", async ({ page }) => {
-    // await page.pause();
-    await page.getByRole("link", { name: "Checking" }).click();
-    await page.getByRole("link", { name: "New Checking" }).click();
-    await page.getByLabel("Standard Checking").check();
-    await page.getByLabel("Individual").check();
-    await page.locator("#name").click();
-    await page.locator("#name").fill("Testkonto");
-    await page.locator("#openingBalance").click();
-    await page.locator("#openingBalance").fill("1000");
-    await page.getByRole("button", { name: " Submit" }).click();
-    await expect(page.locator(""));
-    // await page.getByText("Testkonto", { exact: true }).click();
-  });
-
-  test("min andre test", async ({ page }) => {
-    // await page.pause();
-    await page.getByRole("link", { name: "Checking" }).click();
-    await page.getByRole("link", { name: "New Checking" }).click();
-    await page.getByLabel("Standard Checking").check();
-    await page.getByLabel("Individual").check();
-    await page.locator("#name").click();
-    const kontonavn: String = `Testkonto${randomInt(10) * randomInt(999)}`;
-    await page.locator("#name").fill(`${kontonavn}`);
-  });
-
-  test.afterAll(async ({ page }) => {
-    await page.close();
   });
 });
