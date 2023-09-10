2c9d8cd31e28324a77d44baf7d4e8616c34b5fd4
Henning <henning.kvaloy@gmail.com>
2023-09-10 20:22:12 +0200

    changed url and task

--- tests/bankTest.spec.ts
+++ tests/bankTest.spec.ts
@@ -1,21 +1,17 @@
 import { test, expect } from "@playwright/test";
+import { text } from "stream/consumers";
 test.describe("Min første test Suite", () => {
   //beforeEach logger seg på før hver test som kjøres
   test.beforeEach(async ({ page }) => {
     // Logger på nettbanken.
-    await page.goto(
-      "http://ec2-13-49-246-53.eu-north-1.compute.amazonaws.com:8080/bank/login"
-    );
+    await page.goto("http://13.53.168.89:8080/bank/login");
     await page.getByPlaceholder("Enter User Name").fill("jsmith@demo.io");
     await page.getByPlaceholder("Enter Password").fill("Demo123!");
     await page.getByPlaceholder("Enter Password").press("Enter");
-    // Navigerer til forsiden av banken.
-    await page.goto(
-      "http://ec2-13-49-246-53.eu-north-1.compute.amazonaws.com:8080/bank/home"
-    );
   });
 
   test("Min første test", async ({ page }) => {
-    // Hent kontoinfoen til jsmith fra alle sparekontoer, legg inn tester på suksess og logg beløpet til Console.
+    // Automatiser en test for å sette inn (deposit) penger på kontoen til J .Smith.
+    // Legg til en verifisering som sjekker at testen er vellykket.
   });
 });
