cc8ba894041cee4f58f64a9b8b0963ac10bee7b8
Henning <henning.kvaloy@gmail.com>
2023-09-11 22:09:02 +0200

    added task 2 and a solution suggestion to task 1

--- tests/bankTest.spec.ts
+++ tests/bankTest.spec.ts
@@ -12,42 +12,23 @@ test.describe("Min første test Suite", () => {
   });
 
   test("Min første test", async ({ page }) => {
-    //await page.pause();
+    // Forslag til løsning på oppgave 1
     await page.getByRole("link", { name: "  Checking" }).click();
     await page.getByRole("link", { name: " New Checking" }).click();
     await page.getByLabel("Standard Checking").check();
     await page.getByLabel("Individual").check();
     await page.locator("#name").click();
-    await page.locator("#name").fill("Testkonto98789");
+    const kontonavn: String = `Testkonto${randomInt(10) * randomInt(999)}`;
+    await page.locator("#name").fill(`${kontonavn}`);
     await page.locator("#openingBalance").click();
     await page.locator("#openingBalance").fill("1000");
     await page.getByRole("button", { name: " Submit" }).click();
-    await page.pause();
-    await expect(page.getByText("Testkonto98789")).toBeVisible();
-    // await page.getByText("Testkonto", { exact: true }).click();
+    const testkontoer = await page.getByText(`${kontonavn}`);
+    expect(testkontoer !== undefined).toBeTruthy();
   });
 
   test("min andre test", async ({ page }) => {
-    // await page.pause();
-    await page.getByRole("link", { name: "Checking" }).click();
-    await page.getByRole("link", { name: "New Checking" }).click();
-    await page.getByLabel("Standard Checking").check();
-    await page.getByLabel("Individual").check();
-    await page.locator("#name").click();
-    const kontonavn: String = `Testkonto${randomInt(10) * randomInt(999)}`;
-    await page.locator("#name").fill(`${kontonavn}`);
-  });
-
-  test.only("min tredje test", async ({ page }) => {
-    await page.goto("http://13.53.168.89:8080/bank/account/checking-view");
-    await page.pause();
-    const testkontoer = await page.getByText("Testkonto98789");
-    expect(testkontoer !== undefined).toBeTruthy();
-    //await expect(page.locator( : "Testkonto98789")).toContain("Testkonto98789");
-    // await expect(page.getByText("Testkonto98789")).toBeVisible();
-    //await expect(
-    //  page.getByRole("heading", { name: "Testkonto98789" })
-    //).toBeVisible();
+    //lag en test som setter inn penger på en av kontoene til J. Smith legg til en sjekk på at det ble fullført.
   });
 
   test.afterAll(async ({ page }) => {
