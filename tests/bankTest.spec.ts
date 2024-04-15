882f1e3bfe40fc854581f214ae8a11ff64642002
Henning793 <102154085+Henning793@users.noreply.github.com>
2024-04-15 13:07:36 +0200

    Update url

--- tests/bankTest.spec.ts
+++ tests/bankTest.spec.ts
@@ -5,7 +5,7 @@ import { text } from "stream/consumers";
 test.describe("Min første test Suite", () => {
   test.beforeEach(async ({ page }) => {
     // Logger på nettbanken.
-    await page.goto("http://51.20.79.181:8080/bank/login");
+    await page.goto("http://16.171.175.98:8080/bank");
     await page.getByPlaceholder("Enter User Name").fill("jsmith@demo.io");
     await page.getByPlaceholder("Enter Password").fill("Demo123!");
     await page.getByPlaceholder("Enter Password").press("Enter");
